aaac4015834681a5d442510e2c33ba114f83a9c1
Justin DiRose <justin@justindirose.com>
2019-02-15 15:14:16 -0600

    FEATURE: Hide time tracker controls unless enabled per category [607]

--- plugin.rb
+++ plugin.rb
@@ -6,6 +6,7 @@
 enabled_site_setting :time_tracker_enabled
 
 register_svg_icon "stop" if respond_to?(:register_svg_icon)
+register_svg_icon "clock" if respond_to?(:register_svg_icon)
 
 
 register_asset "stylesheets/time-tracker.scss"
@@ -17,13 +18,34 @@ gem 'logger', '1.3.0'
 gem 'togglv8', '1.2.1'
 load File.expand_path('../lib/time_tracker/engine.rb', __FILE__)
 
-after_initialize {
+after_initialize do
   require_dependency 'guardian'
   class ::Guardian
     include TimeTrackerGuardian
   end  
   
   Category.register_custom_field_type("enable_time_tracker", :boolean)
+  add_to_serializer(:basic_category, :enable_time_tracker) { object.time_tracker_enabled }
+  class ::Category
+    def time_tracker_enabled
+      return false unless SiteSetting.time_tracker_enabled
+
+      if self.custom_fields['enable_time_tracker'] != nil
+        self.custom_fields['enable_time_tracker']
+      end
+    end
+  end
+
+  # Add to topic model
+  require_dependency 'topic'
+  class ::Topic
+    def can_track_time?
+      SiteSetting.time_tracker_enabled && category.time_tracker_enabled
+    end
+  end 
+  # Add to topic view serializer
+  add_to_serializer(:topic_view, :enable_time_tracker) { object.topic.can_track_time? }
+
   User.register_custom_field_type("toggl_api_key", :text)
   
   register_editable_user_custom_field :toggl_api_key
@@ -36,11 +58,4 @@ after_initialize {
       end
     }
   end
-
-  # add_to_serializer(:topic_view, :time_tracker, false) {
-
-  #   tracker = TimeTracker::Tracker.new(id)
-  #   TimeTracker::TrackerSerializer.new(tracker, root: false, scope: scope).as_json
-
-  # }
-}
+end
